a8a89b5ae0dc219dac500fc752bfbcda905a3f07
Elias Souza <elias.ssouza@hotmail.com>
2024-08-06 23:44:57 -0300

    criação do excluir cadastro

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -4,24 +4,39 @@ describe("Cadastro", () => {
     it("Cadastrar uma Entrada", () => {
         cy.visit("https://www.grocerycrud.com/v1.x/demo/bootstrap_theme")
 
-        cy.get('#switch-version-select').select('Bootstrap V4 Theme')
-        cy.contains("Add Customer").click()
-        cy.get('#field-customerName').type('James')
-        cy.get('#field-customerName').should('have.value', 'James')
-        cy.get('#field-contactLastName').type('Dean')
-        cy.get('#field-contactFirstName').type('Joseph')
-        cy.get('#field-phone').type('(99)9 9999-9999')
-        cy.get('#field-addressLine1').type('Rua C, 25')
-        cy.get('#field-city').type('Salvador')
-        cy.get('#field-state').type('Bahia')
-        cy.get('#field-postalCode').type('00000-000')
-        cy.get('#field-country').type('Brasil')
-        cy.get('#field_salesRepEmployeeNumber_chosen').click()
-        cy.get('#field_salesRepEmployeeNumber_chosen .chosen-results li').contains('Bott').click()
-        cy.get('#form-button-save').click()
-        cy.contains('Your data has been successfully stored into the database.').should('be.visible')
-        cy.task('closeBrowser')
+        criarCadastro ()
+
+        //excluirCadastro ()
 
         
     });
 });
+
+function criarCadastro (){
+    cy.get('#switch-version-select').select('Bootstrap V4 Theme')
+    cy.contains("Add Customer").click()
+    cy.get('#field-customerName').type('James')
+    cy.get('#field-customerName').should('have.value', 'James')
+    cy.get('#field-contactLastName').type('Dean')
+    cy.get('#field-contactFirstName').type('Joseph')
+    cy.get('#field-phone').type('(99)9 9999-9999')
+    cy.get('#field-addressLine1').type('Rua C, 25')
+    cy.get('#field-city').type('Salvador')
+    cy.get('#field-state').type('Bahia')
+    cy.get('#field-postalCode').type('00000-000')
+    cy.get('#field-country').type('Brasil')
+    cy.get('#field_salesRepEmployeeNumber_chosen').click()
+    cy.get('#field_salesRepEmployeeNumber_chosen .chosen-results li').contains('Bott').click()
+    cy.get('#form-button-save').click()
+    cy.contains('Your data has been successfully stored into the database.').should('be.visible')
+    cy.task('closeBrowser')
+}
+
+function excluirCadastro (){
+    cy.get('#switch-version-select').select('Bootstrap V4 Theme')
+    cy.get('input[name="customerName"]').type('James')
+    cy.contains('James').should('be.visible')
+    cy.contains('button', 'More').click()
+    cy.contains('button', 'Delete').click()
+    cy.task('closeBrowser')
+}
