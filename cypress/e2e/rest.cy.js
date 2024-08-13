c3cb16286dcc66f46816e5d175e1872679f266df
Elias Souza <elias.ssouza@hotmail.com>
2024-08-12 23:31:58 -0300

    criação do método post

--- cypress/e2e/rest.cy.js
+++ cypress/e2e/rest.cy.js
@@ -4,21 +4,56 @@ describe('Realizar testes de API', () => {
     it('Realizar get simples com retorno positivo', () => {
         cy.request({
             method: 'GET',
-            url: 'https://pokeapi.co/api/v2/ability/?limit=20&offset=20',
-        }).then(res => {
+            url: 'https://jsonplaceholder.typicode.com/posts',
+        }).then((res) => {
             console.log(res)
-        })  
+            expect(res.status).to.equal(200)
+            expect(res.status).to.exist
+            })  
     })
 
     it('Realizar get simples com retorno negativo', () => {
         cy.request({
             method: 'GET',
-            url: 'https://developer.marvel.com/get/v1/public/characters',
+            url: 'https://jsonplaceholder.typicode.com/comments',
         }).then(res => {
              console.log(res)
+             expect(res.status).to.equal(401)
         })  
     })
 
+    it('Realizar um post com retorno positivo', () =>{
+        cy.request({
+            method: 'POST',
+            url:'https://jsonplaceholder.typicode.com/posts',
+            body: {
+                'userId':'11',
+                'id': '101',
+                'title': 'testing',
+                'body': 'just testing but I will delete it soon'
+            }
+        }).then(res =>{
+            console.log(res)
+            expect(res.status).to.equal(201)
+        })
+    })
+
+    it.only('Realizar um post com retorno negativo', () =>{
+        cy.request({
+            method: 'POST',
+            url:'https://jsonplaceholder.typicode.com/posts',
+            body: {
+                'userId':'',
+                'id': '',
+                'title': '',
+                'body': '',
+            }
+        }).then(res =>{
+            console.log(res)
+            //expect(res.status).to.equal(201)
+            expect(res.status).to.not.be.empty
+        })
+    })
     
     
 });
