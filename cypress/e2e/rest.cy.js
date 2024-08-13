b165ba386e76bb1550f6463ad062acda157effaa
Elias Souza <elias.ssouza@hotmail.com>
2024-08-12 23:50:58 -0300

    métodos put e delete

--- cypress/e2e/rest.cy.js
+++ cypress/e2e/rest.cy.js
@@ -12,13 +12,14 @@ describe('Realizar testes de API', () => {
             })  
     })
 
-    it('Realizar get simples com retorno negativo', () => {
+    it.only('Realizar get simples com retorno negativo', () => {
         cy.request({
             method: 'GET',
-            url: 'https://jsonplaceholder.typicode.com/comments',
+            url: 'https://jsonplaceholder.typicode.com/comments/501',
+            failOnStatusCode: false,
         }).then(res => {
              console.log(res)
-             expect(res.status).to.equal(401)
+             expect(res.status).to.equal(404)
         })  
     })
 
@@ -38,23 +39,31 @@ describe('Realizar testes de API', () => {
         })
     })
 
-    it.only('Realizar um post com retorno negativo', () =>{
+    it('Realizar um put com retorno positivo', () =>{
         cy.request({
-            method: 'POST',
-            url:'https://jsonplaceholder.typicode.com/posts',
+            method: 'PUT',
+            url:'https://jsonplaceholder.typicode.com/posts/100',
             body: {
-                'userId':'',
-                'id': '',
-                'title': '',
-                'body': '',
+                'title': 'testing',
+                'body': 'just testing but I will delete it soon'
             }
         }).then(res =>{
             console.log(res)
-            //expect(res.status).to.equal(201)
-            expect(res.status).to.not.be.empty
+            expect(res.status).to.equal(200)
+            //expect(res.status).to.not.be.empty
         })
     })
-    
+
+    it('Realizar um delete com retorno positivo', () =>{
+        cy.request({
+            method: 'DELETE',
+            url:'https://jsonplaceholder.typicode.com/posts/100',
+        }).then(res =>{
+            console.log(res)
+            expect(res.status).to.equal(200)
+            //expect(res.status).to.not.be.empty
+        })
+    })
     
 });
 
